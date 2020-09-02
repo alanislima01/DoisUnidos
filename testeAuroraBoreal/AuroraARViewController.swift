37cc54d08dbb61771ae6b63916b13fdde5acb350
Alanis <alsc@academy.cin.ufpe.br>
2020-09-02 13:44:24 -0300

    ARstoryboard e AR

--- testeAuroraBoreal/AuroraARViewController.swift
+++ testeAuroraBoreal/AuroraARViewController.swift
@@ -18,23 +18,28 @@ class AuroraARViewController: UIViewController{
     
     let configuration = ARWorldTrackingConfiguration() //para rastrear a posicao e orientacao do dispositivo em relacao ao mundo real.. porem n aparece a opcao ARWorldTrackingSessionConfiguration
     
-    @IBOutlet weak var sceneView: ARSCNView!
     
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+
     //essa acao de baixo eh um nó..(uma posicao no espaco que nao tem forma nem tamanho)... Aqui estamos fazendo com que esse no venha para a nossa camera
     @IBAction func add(_ sender: Any) {
+        let node = SCNNode()
 
       //  node.geometry = SCNTorus(ringRadius: 0.3, pipeRadius: 0.1) //ringRadius é o tamanho horizontal / pipeR é a grossura do anel.. Ring sempre deve ser maior qe o pipe.. é a rosquinha hihi
      //   node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3) //aqui para ser um cilindro, por exemplo.. existem varias coisas
 //        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)//aqui vamos colocar o tamanho do no e dizer que eh uma caixa!
-        let path = UIBezierPath() //para criar formas personalizadas a partir de um caminho q eu escolhjo
-        path.move(to: CGPoint(x: 0, y:0)) //ele vai estar 0 metros de onde vc se posicionou
-        path.addLine(to: CGPoint(x: 0, y: 0.2))
+//        let path = UIBezierPath() //para criar formas personalizadas a partir de um caminho q eu escolhjo:
+//        path.move(to: CGPoint(x: 0, y:0)) //ele vai estar 0 metros de onde vc se posicionou
+//        path.addLine(to: CGPoint(x: 0, y: 0.2))
+//        path.addLine(to: CGPoint(x:0.2 , y:0.3))// vai de 0 ao ponto 0.2 para direita e 0.1 acima
+//        let shape = SCNShape(path: path, extrusionDepth: 0.2)
+//        node.geometry = shape
         
-        let node = SCNNode()
-        //node.geometry = SCNPlane(width: 0.2, height: 0.2)
+        node.geometry = SCNPlane(width: 0.2, height: 0.2)
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.purple
         node.geometry?.firstMaterial?.specular.contents = UIColor.white//luz que vai refletir da caixa
-        node.position = SCNVector3(0,0,-0.3)//ele 0.3 é para deixar ele distante do vetor
+        node.position = SCNVector3(0.2,0.3,-0.2)//ele 0.3 é para deixar ele distante do vetor
         self.sceneView.scene.rootNode.addChildNode(node)
         //aqui em cima eu criei a caixa
         
@@ -92,7 +97,7 @@ class AuroraARViewController: UIViewController{
                     } // o for ta lapidando ainda melhor as info que realmente precisam (so selecionando as imagens)
                     
                     print(self.image[0])//para mostrar o elemento 0 desse array
-                  //  self.loadImageAPI(url: self.image[0].url) //aqui eu vou ta chamando o item zero para mostrar a imagem!! DEPOIS TIRAR DO COMENTARIO!!!
+                    self.loadImageAPI(url: self.image[0].url) //aqui eu vou ta chamando o item zero para mostrar a imagem!! DEPOIS TIRAR DO COMENTARIO!!!
                     
                     //para depois eu mostrar a imagem uso isso de baixar a imagem e depois colocar o resultado disso imageView!!!!
                     
@@ -107,18 +112,14 @@ class AuroraARViewController: UIViewController{
     }
     
     // tenho que criar um func loadImage para poder realmente baixar a imagem.
-//
-//    func loadImageAPI(url: String){ //baixar a imagem do elemento
-//        let urlImage = URL(string: url)
-//        let session = URLSession.shared
-//        let task = session.dataTask(with: urlImage!) { data, response , error in
-//           if data! = tru
-//            UIImage(data: data!) //coloco esse UIImage na ImageView para ele aparecer!!!!!!!! //mas antes devo criar um if
-//        }
-//        task.resume()
-//    }
-//
-//
+
+    func loadImageAPI(url: String){ //baixar a imagem do elemento
+        let urlImage = URL(string: "https://odia.ig.com.br/_midias/jpg/2020/03/18/cao-16275945.jpg") //
+        let data = try? Data(contentsOf: urlImage!)
+        let image = UIImage(data: data!)
+    }
+
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- testeAuroraBoreal/ViewController.swift
+++ testeAuroraBoreal/ViewController.swift
@@ -1,21 +1,26 @@
 //
 //  ViewController.swift
-//  testeAuroraBoreal
+//  testeARKit
 //
-//  Created by Alanis Lima Santa Clara on 27/08/20.
+//  Created by Alanis Lima Santa Clara on 01/09/20.
 //  Copyright © 2020 Alanis Lima Santa Clara. All rights reserved.
 //
 
 import UIKit
-
+import ARKit
 
 class ViewController: UIViewController {
-
+    
+    
+    let configuration = ARWorldTrackingConfiguration() //para rastrear a posicao e orientacao do dispositivo em relacao ao mundo real.. porem n aparece a opcao ARWorldTrackingSessionConfiguration
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
