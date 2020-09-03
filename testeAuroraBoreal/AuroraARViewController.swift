ce89af909e419ee868ce87fba822e7acd559fccb
Alanis <alsc@academy.cin.ufpe.br>
2020-09-03 14:33:31 -0300

    correcao da ordenacao da lista e apresentacao da esfera

--- testeAuroraBoreal/AuroraARViewController.swift
+++ testeAuroraBoreal/AuroraARViewController.swift
@@ -14,34 +14,77 @@ import ARKit
 
 class AuroraARViewController: UIViewController{
     
-    var image: [ImageAurora] = [] // pode ter ou nao ago dentro.. nao necesariamente vai ter um image aurora, de 1a vai ser um nil
+    var aurorasImages: [ImageAurora] = [] // pode ter ou nao ago dentro.. nao necesariamente vai ter um image aurora, de 1a vai ser um nil
     
     let configuration = ARWorldTrackingConfiguration() //para rastrear a posicao e orientacao do dispositivo em relacao ao mundo real.. porem n aparece a opcao ARWorldTrackingSessionConfiguration
     
     
     
     @IBOutlet weak var sceneView: ARSCNView!
-
+    
     //essa acao de baixo eh um nó..(uma posicao no espaco que nao tem forma nem tamanho)... Aqui estamos fazendo com que esse no venha para a nossa camera
     @IBAction func add(_ sender: Any) {
-        let node = SCNNode()
-
-      //  node.geometry = SCNTorus(ringRadius: 0.3, pipeRadius: 0.1) //ringRadius é o tamanho horizontal / pipeR é a grossura do anel.. Ring sempre deve ser maior qe o pipe.. é a rosquinha hihi
-     //   node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3) //aqui para ser um cilindro, por exemplo.. existem varias coisas
-//        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)//aqui vamos colocar o tamanho do no e dizer que eh uma caixa!
-//        let path = UIBezierPath() //para criar formas personalizadas a partir de um caminho q eu escolhjo:
-//        path.move(to: CGPoint(x: 0, y:0)) //ele vai estar 0 metros de onde vc se posicionou
-//        path.addLine(to: CGPoint(x: 0, y: 0.2))
-//        path.addLine(to: CGPoint(x:0.2 , y:0.3))// vai de 0 ao ponto 0.2 para direita e 0.1 acima
-//        let shape = SCNShape(path: path, extrusionDepth: 0.2)
-//        node.geometry = shape
         
-        node.geometry = SCNPlane(width: 0.2, height: 0.2)
-        node.geometry?.firstMaterial?.diffuse.contents = UIColor.purple
-        node.geometry?.firstMaterial?.specular.contents = UIColor.white//luz que vai refletir da caixa
-        node.position = SCNVector3(0.2,0.3,-0.2)//ele 0.3 é para deixar ele distante do vetor
-        self.sceneView.scene.rootNode.addChildNode(node)
-        //aqui em cima eu criei a caixa
+        
+        //  node.geometry = SCNTorus(ringRadius: 0.3, pipeRadius: 0.1) //ringRadius é o tamanho horizontal / pipeR é a grossura do anel.. Ring sempre deve ser maior qe o pipe.. é a rosquinha hihi
+        //   node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3) //aqui para ser um cilindro, por exemplo.. existem varias coisas
+        //        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)//aqui vamos colocar o tamanho do no e dizer que eh uma caixa!
+        //        let path = UIBezierPath() //para criar formas personalizadas a partir de um caminho q eu escolhjo:
+        //        path.move(to: CGPoint(x: 0, y:0)) //ele vai estar 0 metros de onde vc se posicionou
+        //        path.addLine(to: CGPoint(x: 0, y: 0.2))
+        //        path.addLine(to: CGPoint(x:0.2 , y:0.3))// vai de 0 ao ponto 0.2 para direita e 0.1 acima
+        //        let shape = SCNShape(path: path, extrusionDepth: 0.2)
+        //        node.geometry = shape
+        
+        //let imageAppear = UIImage(named: "Yuumi")
+        
+        //let node = SCNNode(geometry: SCNPlane(width: 0.3, height: 0.3))
+        
+        let node = SCNNode()
+        //node.geometry = SCNTorus(ringRadius: 0.3, pipeRadius: 0.1)
+        node.geometry = SCNSphere(radius: 0.15)
+        // node.geometry = SCNPlane(width: 0.3, height: 0.3)
+        
+        // node.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Yuumi")
+        
+        
+        //        node.geometry?.firstMaterial?.diffuse.contents = UIColor.purple
+        node.geometry?.firstMaterial?.specular.contents = UIColor.purple//luz que vai refletir da caixa
+        node.position = SCNVector3(0,0.4,0.2)//ele 0.3 é para deixar ele distante do vetor
+        
+        //        //aqui em cima eu criei a caixa
+        
+        
+        DispatchQueue.global().async {
+            
+            print(self.aurorasImages.count)
+            
+            let imagemFinal = self.aurorasImages[0]
+            
+            //  let elementoAleatorio =   self.aurorasImages.randomElement()
+            //print(elementoAleatorio)
+            do{
+                var url = imagemFinal.url.replacingOccurrences(of: "http:", with: "https:")
+                
+                print("Nova URL É " + url)
+                var urlImage = URL(string: url)
+                
+                if let urlData = try? Data(contentsOf: urlImage!){
+                    
+                    let image = UIImage(data: urlData)
+                    
+                    DispatchQueue.main.async {
+                        
+                        node.geometry?.firstMaterial?.diffuse.contents = image
+                        self.sceneView.scene.rootNode.addChildNode(node)
+                        
+                    }
+                }
+            }catch{
+                print("erro")
+            }
+            
+        }
         
     }
     
@@ -57,10 +100,10 @@ class AuroraARViewController: UIViewController{
         self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
     
-    func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat{
-        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
-    }
-    
+    //    func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat{
+    //        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
+    //    }
+    //
     
     
     
@@ -71,7 +114,7 @@ class AuroraARViewController: UIViewController{
         load()
         
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
-        self.sceneView.session.run(configuration) //descobrir minha posicao
+        self.sceneView.session.run(configuration) //descobrir minha posicao no mundo hihi
         self.sceneView.autoenablesDefaultLighting = true  // adcionamos a iluminacao
         
         
@@ -85,41 +128,38 @@ class AuroraARViewController: UIViewController{
         let task = session.dataTask(with: url!) { data, response , error in
             do{
                 let decoder = JSONDecoder()
-                let jsonData = try decoder.decode(Welcome.self, from: data!) //Tem que ser o que ta vindo da internet o "super json". Tem que forcar o data que vem da linha 25(q se diz opcional)... ta transformando oq da internet no tipo de dados do FileStruct.
                 
-                DispatchQueue.main.async { //faz funcao assincrona, ou seja, espera algo acontecer para rodar dps
-                    //vamos criar um laço
-                    for aurora in jsonData.images.values {
-                        
-                        self.image.append(aurora) //ele vai percorrer o diciniocario e vai salvar as coisas do array
-                        
-                        //       print(aurora)
-                    } // o for ta lapidando ainda melhor as info que realmente precisam (so selecionando as imagens)
-                    
-                    print(self.image[0])//para mostrar o elemento 0 desse array
-                    self.loadImageAPI(url: self.image[0].url) //aqui eu vou ta chamando o item zero para mostrar a imagem!! DEPOIS TIRAR DO COMENTARIO!!!
-                    
-                    //para depois eu mostrar a imagem uso isso de baixar a imagem e depois colocar o resultado disso imageView!!!!
-                    
-                    // Vou ter que descobrir como pegar as informacoes de um dicionario e colocar numa self.image... key e valor.. como fazer o valor da chave nessa
-                    
-                }
+                let jsonData = try decoder.decode(Welcome.self, from: data!) //Tem que ser o que ta vindo da internet o "super json".
+                
+                self.aurorasImages = Array(jsonData.images.values)
+                self.aurorasImages = self.aurorasImages.filter { $0.category == Category.cam}
+                
+                self.aurorasImages = self.aurorasImages.sorted(by: {$0.url>$1.url}) //vou ordenar minha lista a partir do criterio de url(1o com o segundo, compara por ordem alfabetica)
+                
+                
             }catch{
-                print("Deu erro aqui")
+                print("Erro: \(error.localizedDescription)")
+                
+                
             }
         }
         task.resume()
     }
     
     // tenho que criar um func loadImage para poder realmente baixar a imagem.
-
-    func loadImageAPI(url: String){ //baixar a imagem do elemento
-        let urlImage = URL(string: "https://odia.ig.com.br/_midias/jpg/2020/03/18/cao-16275945.jpg") //
-        let data = try? Data(contentsOf: urlImage!)
-        let image = UIImage(data: data!)
-    }
-
-
+    
+    //    func loadImageAPI(url: String){ //baixar a imagem do elemento
+    //        let urlImage = URL(string: "https://odia.ig.com.br/_midias/jpg/2020/03/18/cao-16275945.jpg") //
+    //        let data = try? Data(contentsOf: urlImage!)
+    //        let image = UIImage(data: data!)
+    //    }
+    
+    
+    
+    
+    
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
